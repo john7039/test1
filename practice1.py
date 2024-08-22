fac1b50b9fb37155de1926173aa87d817c7de9c1
john7039 <john.joo@icloud.com>
2024-08-22 16:34:18 +0900

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,27 @@
+# 문제: 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오
+# 백준 2번
+# n = int(input("정수 A 입력: "))
+# n1 = int(input("정수 B 입력: "))
+
+# if n > n1:
+#     print(">")
+# elif n < n1:
+#     print("<")
+# elif n == n1:
+#     print("==")
+
+
+# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 
+# 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+# n = int(input("시험 점수: "))
+# if n< 60:
+#     print("F")
+# elif 60 <= n < 70:
+#     print("D")
+# elif 70 <= n < 80:
+#     print("C")
+# elif 80 <= n < 90:
+#     print("B")
+# elif n >= 90:
+#     print("A")
